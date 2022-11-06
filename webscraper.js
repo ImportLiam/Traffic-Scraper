162f5038cf40fae9c40684b0016f9c42a9c08336
ImportLiam <73508475+ImportLiam@users.noreply.github.com>
2022-11-06 16:31:54 -0600

    first commit

--- webscraper.js
+++ webscraper.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const cheerio = require('cheerio');
-
+//first
 request('https://pof-usa.com/firearm/revolution/', (error,response, html) => {
 
   if(!error && response.statusCode == 200) {
